b17352ab57f51dd428f92c796e4a0f4d7918e1f2
Pascal Wittler <git@pascalwittler.de>
2022-05-09 04:33:45 +0200

    add autoload function

--- index.php
+++ index.php
@@ -5,3 +5,9 @@ ini_set('display_errors', '1');
 if (version_compare(PHP_VERSION, '8.1.0', '<')) {
     die('This application needs PHP >= 8.1.0 but your PHP version is ' . PHP_VERSION . '.');
 }
+
+function autoload($class_name) {
+    require_once(__DIR__ . '/classes/' . $class_name . '.php');
+}
+
+spl_autoload_register('autoload');
